8aac139f51d0b0d991a2621a32f0db3f25bbf452
imranmatano6@gmail.com <you@example.com>
2023-03-07 09:19:10 +0300

    Modiefied all the files

--- TriomegaB.js
+++ TriomegaB.js
@@ -1,6 +1,7 @@
 const ope = document.getElementById('ope');
 const slide = document.querySelector('.slide');
 const clo = document.getElementById('clo');
+const toTop = document.querySelector('.to-top');
 
 ope.addEventListener('click', function(){
     slide.style.width = '300px';
@@ -8,4 +9,13 @@ ope.addEventListener('click', function(){
 });
 clo.addEventListener('click', function(){
     slide.style.width = '0', display = 'none';
+});
+
+window.addEventListener('scroll', () =>{
+    if(window.pageYOffset > 100){
+        toTop.classList.add('active');
+    }
+    else{
+        toTop.classList.remove('active');
+    }
 });
